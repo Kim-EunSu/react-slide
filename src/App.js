d83abe3f53ce945e44b60094505783bc0dd974e6
Kim-EunSu <kes73640@gmail.com>
2022-12-03 16:02:53 +0900

    1203

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/layout/Header";
+import Swiper from "./components/layout/Swiper";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Swiper />
     </div>
   );
 };
---
+++ src/components/layout/Swiper.js
@@ -0,0 +1,38 @@
+// import React, { useRef, useState } from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../css/swiper.css";
+
+// import required modules
+import { Keyboard, Pagination, Navigation } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Keyboard, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
